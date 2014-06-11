e8cd8ccc355a690fbe2f1cd22896ba0614b37ca5
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 13:02:56 -0700

    initial commit

--- game.py
+++ game.py
@@ -11,13 +11,80 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+    SOLID = True
+
+class Character(GameElement):
+    IMAGE = "Girl"
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    #Rock Positions
+    rock_positions = [
+        (2,1),
+        (1,2),
+        (3,2),
+        (2,3)
+    ]
+    rocks = []
+
+    #Rocks
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+    
+    for rock in rocks:
+        print rock
+
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2, 2, PLAYER)
+    print PLAYER
+
+    #Message from Sponsors
+    GAME_BOARD.draw_msg("This game is wicked awesome")
+
+def keyboard_handler():
+    direction = None
+
+    if KEYBOARD[key.UP]:
+        direction = "up"
+    elif KEYBOARD[key.RIGHT]:
+        direction = "right"
+    elif KEYBOARD[key.LEFT]:
+        direction = "left"
+    elif KEYBOARD[key.DOWN]:
+        direction = "down"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+        print type(existing_el)
+
+        if existing_el is None or not existing_el.SOLID:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
