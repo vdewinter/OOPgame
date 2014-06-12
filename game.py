3a7e29189e188386144a9ec0d36004f4c9b147f2
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 18:14:04 -0700

    game logic enhanced

--- game.py
+++ game.py
@@ -11,17 +11,65 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 5
-GAME_HEIGHT = 5
+GAME_WIDTH = 9
+GAME_HEIGHT = 9
 
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
 
+class Gem(GameElement):
+    SOLID = False   
+
+    def __init__(self, color):
+        self.color = color
+        if self.color == "orange":
+            self.IMAGE = "OrangeGem"
+        if self.color == "blue":
+            self.IMAGE = "BlueGem"
+
+    def interact(self, player):
+        if self.color == "orange":
+            player.inventory.append("orange_gem")
+        if self.color == "blue":
+            player.inventory.append("blue_gem")
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" % (len(player.inventory)))
+
+class Key(GameElement):
+
+    IMAGE = "Key"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append("key")
+        GAME_BOARD.draw_msg("You just acquired a key! Open the door next.")
+
+class Chest(GameElement):
+    IMAGE = "Chest"
+    SOLID = True
+
+    def interact(self, player):
+        if "blue_gem" in player.inventory and "orange_gem" in player.inventory:
+            self.SOLID = False
+            GAME_BOARD.draw_msg("You opened the chest! Go finish the game.")   
+
+class Star(GameElement):
+    IMAGE = "Star"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append("Star")
+        GAME_BOARD.draw_msg("You got the Star! You have won the game!")        
+
 class Character(GameElement):
     IMAGE = "Girl"
+    SOLID = True
 
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+        
     def next_pos(self, direction):
         if direction == "up":
             return (self.x, self.y-1)
@@ -32,16 +80,101 @@ class Character(GameElement):
         elif direction == "right":
             return (self.x+1, self.y)
         return None
+
+# class Computer(Character):
+#     IMAGE = "Horns"
+
+class Door(GameElement):
+    IMAGE = "DoorClosed"
+    SOLID = True
+
+    def interact(self, player):
+        if "key" in player.inventory:
+            self.SOLID = False
+            GAME_BOARD.draw_msg("You opened the door!")
+
+class Axe(GameElement):
+    IMAGE = "Axe"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append("axe")
+        GAME_BOARD.draw_msg("You just acquired an axe! Chop down the tree.")
+
+class Tree(GameElement):
+    IMAGE = "TallTree"
+    SOLID = True
+
+    def interact(self, player):
+        if "axe" in player.inventory:
+            self.SOLID = False
+            GAME_BOARD.draw_msg("You cut down the tree!")
+
+class Princess(GameElement):
+    IMAGE = "Princess"
+    SOLID = True
+
+    def interact(self, player):
+        if "orange_gem" in player.inventory:
+            self.SOLID = False
+            GAME_BOARD.draw_msg("You bribed the Princess in letting you pass!")
+
+class CatTreat(GameElement):
+    IMAGE = "Cat_Treat"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append("Cat_Treat")
+        GAME_BOARD.draw_msg("You just acquired a cat treat!")
+
+class Cat(GameElement):
+    IMAGE = "Cat"
+    SOLID = True
+
+    def interact(self, player):
+        if "Cat_Treat" in player.inventory:
+            self.SOLID = False
+            GAME_BOARD.draw_msg("You have given the kitten a treat! Now it will let you through")
+
 ####   End class definitions    ####
+
 
 def initialize():
     """Put game initialization code here"""
     #Rock Positions
     rock_positions = [
-        (2,1),
-        (1,2),
+        (0,0),
+        (0,1),
+        (0,2),
+        (1,0),
+        (1,4),
+        (2,0),
+        (2,2),
+        (2,4),
+        (2,6),
+        (2,7),
         (3,2),
-        (2,3)
+        (3,4),
+        (3,7),
+        (4,1),
+        (4,2),
+        (4,4),
+        (4,5),
+        (4,6),
+        (4,7),
+        (5,1),
+        (6,1),
+        (6,3),
+        (6,5),
+        (6,7),
+        (7,1),
+        (7,2),
+        (7,3),
+        (7,5),
+        (7,6),
+        (7,7),
+        (7,8),
+        (8,1)
     ]
     rocks = []
 
@@ -49,20 +182,75 @@ def initialize():
     for pos in rock_positions:
         rock = Rock()
         GAME_BOARD.register(rock)
-        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        GAME_BOARD.set_el(pos[1], pos[0], rock)
         rocks.append(rock)
-    
-    for rock in rocks:
-        print rock
 
     global PLAYER
     PLAYER = Character()
+    # COMPUTER = Computer()
     GAME_BOARD.register(PLAYER)
-    GAME_BOARD.set_el(2, 2, PLAYER)
+    GAME_BOARD.set_el(0, 8, PLAYER)
     print PLAYER
+    # GAME_BOARD.register(COMPUTER)
+    # GAME_BOARD.set_el(8, 7, COMPUTER)
+    # print COMPUTER
 
     #Message from Sponsors
-    GAME_BOARD.draw_msg("This game is wicked awesome")
+    GAME_BOARD.draw_msg("Goal: get the star.")
+
+    #Gems
+    orange_gem = Gem("orange")
+    GAME_BOARD.register(orange_gem)
+    GAME_BOARD.set_el(6, 3, orange_gem)
+
+    blue_gem = Gem("blue")
+    GAME_BOARD.register(blue_gem)
+    GAME_BOARD.set_el(8, 6, blue_gem)
+
+    #Keys
+    key = Key()
+    GAME_BOARD.register(key)
+    GAME_BOARD.set_el(0, 7, key)
+
+    #Cat Treat
+    cat_treat = CatTreat()
+    GAME_BOARD.register(cat_treat)
+    GAME_BOARD.set_el(2, 6, cat_treat)
+
+    #Axe
+    axe = Axe()
+    GAME_BOARD.register(axe)
+    GAME_BOARD.set_el(0, 3, axe)
+
+    #Chest
+    chest = Chest()
+    GAME_BOARD.register(chest)
+    GAME_BOARD.set_el(6, 8, chest)
+
+    #Door
+    door = Door()
+    GAME_BOARD.register(door)
+    GAME_BOARD.set_el(0, 6, door)
+
+    #Tree
+    tree = Tree()
+    GAME_BOARD.register(tree)
+    GAME_BOARD.set_el(1, 2, tree)
+
+    #Princess
+    princess = Princess()
+    GAME_BOARD.register(princess)
+    GAME_BOARD.set_el(3, 4, princess)
+
+    #Cat
+    cat = Cat()
+    GAME_BOARD.register(cat)
+    GAME_BOARD.set_el(5, 5, cat)
+
+    #Prize
+    star = Star()
+    GAME_BOARD.register(star)
+    GAME_BOARD.set_el(8, 8, star)
 
 def keyboard_handler():
     direction = None
@@ -82,9 +270,13 @@ def keyboard_handler():
         next_y = next_location[1]
 
         existing_el = GAME_BOARD.get_el(next_x, next_y)
-        print type(existing_el)
+
+        if existing_el:
+            existing_el.interact(PLAYER)
 
         if existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
             GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
+
 
